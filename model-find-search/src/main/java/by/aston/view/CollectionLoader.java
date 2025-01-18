6822096ce93e312b5b0dd6e6b90ffa01f000194d
Vadim Belyakhovich <vadik123belyahovich@gmail.com>
2025-01-18 21:58:10 +0300

    Исправлена генерация случайных коллекций

--- model-find-search/src/main/java/by/aston/view/CollectionLoader.java
+++ model-find-search/src/main/java/by/aston/view/CollectionLoader.java
@@ -10,7 +10,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class CollectionLoader {
-
+    private final static int USER_INPUT_NUMBER_ERROR = -1;
     private final DataInput input;
 
     public CollectionLoader(DataInput input) {
@@ -18,10 +18,7 @@ public class CollectionLoader {
     }
 
     public List<?> getObjectList() {
-        RandomObjectGenerator randomObjectGenerator = new RandomObjectGenerator();
-
         while (true) {
-            //todo записать это метод inputOfData?
             input.showMessage("""
                     Выберите действие:
                     1. Создать коллекцию объектов вручную.
@@ -29,108 +26,64 @@ public class CollectionLoader {
                     3. Считать коллекцию из файла.
                     4. Выход.
                     """);
+            try {
+                var choice = NumberUtils.parseInt(input.readLine());
 
-            int choice = NumberUtils.parseInt(input.readLine());//todo try везде
-
-            switch (choice) {
-                case 1 -> {
-                    var keyboardInput = new KeyboardInput(input);
-                    return keyboardInput.getObjectList();
+                switch (choice) {
+                    case 1 -> {
+                        var keyboardInput = new KeyboardInput(input);
+                        return keyboardInput.getObjectList();
+                    }
+                    case 2 -> {
+                        return generateRandomCollection();
+                    }
+                    case 3 -> {
+                        return readCollectionFromFile();
+                    }
+                    case 4 -> {
+                        return Collections.emptyList();
+                    }
+                    default -> input.showMessage("Неверный выбор. Пожалуйста, попробуйте снова.");
                 }
-                case 2 -> {
-                    return generateRandomCollection(randomObjectGenerator);//todo
-                }
-                case 3 -> {
-                    return readCollectionFromFile();
-                }
-                case 5 -> {
-                    return Collections.emptyList();
-                }
-                default -> input.showMessage("Неверный выбор. Пожалуйста, попробуйте снова.");
+            } catch (NumberFormatException exception) {
+                input.showErrorMessage("Ошибка формата числа при выборе действия:" +
+                        exception.getMessage());
             }
-
-
-//            try {
-//                switch (choice) {
-//                    case 1:
-//                        keyboardInput.userDialogInit();
-//                        break;
-//                    case 2:
-//                        generateRandomCollection(randomObjectGenerator);
-//                        break;
-//
-//                    case 3:
-//                        readCollectionFromFile(fileHandler, keyboardInput);
-//                        break;
-//
-//                    case 4:
-//                        writeCollectionToFile(fileHandler, keyboardInput, carCollection, bookCollection, vegetableCollection);
-//                        break;
-//
-//                    case 5:
-//                        input.showMessage("Выход из программы.");
-//                        return;
-//
-//                    default:
-//                        input.showMessage("Неверный выбор. Пожалуйста, попробуйте снова.");
-//                }
-//            } catch (Exception e) {
-//                input.showMessage("Произошла ошибка: " + e.getMessage());
-//            }
         }
     }
 
-    private <T> List<T> generateRandomCollection(RandomObjectGenerator randomObjectGenerator) {
+    private List<?> generateRandomCollection() {
         try {
-            input.showMessage("""
-                    Выберите тип коллекции для генерации:
-                    1. Автомобили
-                    2. Книги
-                    3. Корнеплоды
-                    """);
-            int userObjectChoice = NumberUtils.parseInt(input.readLine());
-
             input.showMessage("Введите количество элементов для генерации: ");
-            int userCountOfObjectChoice = NumberUtils.parseInt(input.readLine());
-
-            List<?> randomCollection = null;
+            var userCountChoice = NumberUtils.parseInt(input.readLine());
+            var userObjectChoice = userCollectionChoice();
 
             switch (userObjectChoice) {
-                case 1:
-                    randomCollection = randomObjectGenerator.getObjectList(Car.class, userCountOfObjectChoice);
-                    input.showMessage("Сгенерированные автомобили: " + randomCollection);
-                    break;
-                case 2:
-                    randomCollection = randomObjectGenerator.getObjectList(Book.class, userCountOfObjectChoice);
-                    input.showMessage("Сгенерированные книги: " + randomCollection);
-                    break;
-                case 3:
-                    randomCollection = randomObjectGenerator.getObjectList(Vegetable.class, userCountOfObjectChoice);
-                    input.showMessage("Сгенерированные корнеплоды: " + randomCollection);
-                    break;
-                default:
-                    input.showMessage("Неверный тип объекта.");
-                    break;
+                case 1 -> {
+                    return generateAndDisplayCollection(Car.class, userCountChoice);
+                }
+                case 2 -> {
+                    return generateAndDisplayCollection(Book.class, userCountChoice);
+                }
+                case 3 -> {
+                    return generateAndDisplayCollection(Vegetable.class, userCountChoice);
+                }
+                default -> {
+                    input.showErrorMessage("Неверный выбор типа коллекции.");
+                    return Collections.emptyList();
+                }
             }
-            //todo
-//            return randomCollection;
         } catch (NumberFormatException exception) {
-            input.showErrorMessage(exception.getMessage());
+            input.showErrorMessage("Ошибка формата числа при выборе количество элементов:" +
+                    exception.getMessage());
         }
         return Collections.emptyList();
     }
 
     private List<?> readCollectionFromFile() {
-        input.showMessage("""
-                Выберите тип коллекции для чтения:
-                1. Автомобили
-                2. Книги
-                3. Корнеплоды
-                """);
-        var userObjectChoice = NumberUtils.parseInt(input.readLine());
-
         input.showMessage("Введите путь к файлу для чтения: ");
         var readFilePath = input.readLine();
+        var userObjectChoice = userCollectionChoice();
 
         switch (userObjectChoice) {
             case 1 -> {
@@ -149,16 +102,37 @@ public class CollectionLoader {
         }
     }
 
+    private <T> List<T> generateAndDisplayCollection(Class<T> clazz, int objectCount) {
+        List<T> collection = RandomObjectGenerator.getObjectList(clazz, objectCount);
+        input.showMessage("Случайно сгенерированная коллекция: " + collection);
+        return collection;
+    }
+
     private <T> List<T> readAndDisplayCollection(String filePath, Class<T> clazz) {
         try {
             List<T> collection = FileHandler.readCollectionFromFile(filePath, clazz);
             input.showMessage("Считанная из файла коллекция: " + collection);
             return collection;
         } catch (IOException | ClassNotFoundException exception) {
-            input.showErrorMessage("При загрузки коллекции возникла ошибка: " + exception.getMessage());
+            input.showErrorMessage("При загрузки коллекции возникла ошибка: " +
+                    exception.getMessage());
             return Collections.emptyList();
         }
     }
 
-
+    private int userCollectionChoice() {
+        try {
+            input.showMessage("""
+                    Выберите тип коллекции для чтения:
+                    1. Автомобили
+                    2. Книги
+                    3. Корнеплоды
+                    """);
+            return NumberUtils.parseInt(input.readLine());
+        } catch (NumberFormatException exception) {
+            input.showErrorMessage("Ошибка формата числа при выборе коллекции: " +
+                    exception.getMessage());
+            return USER_INPUT_NUMBER_ERROR;
+        }
+    }
 }
--- model-find-search/src/main/java/by/aston/view/RandomObjectGenerator.java
+++ model-find-search/src/main/java/by/aston/view/RandomObjectGenerator.java
@@ -1,75 +1,89 @@
 package by.aston.view;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import by.aston.model.Book;
 import by.aston.model.Car;
 import by.aston.model.Vegetable;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class RandomObjectGenerator {
 
     private static final Random random = new Random();
+    private final static String PATH_BOOK_AUTHORS = "randomazers/names.txt";
+    private final static String PATH_BOOK_TITLES = "randomazers/titles.txt";
+    private final static String PATH_CAR_MODELS = "randomazers/models.txt";
+    private final static String PATH_CAR_COLORS = "randomazers/colors.txt";
+    private final static String PATH_VEGETABLES_NAME = "randomazers/vegetables.txt";
 
-    public static <T> List<T> getObjectList(Class<T> clazz, int count){
+    public static <T> List<T> getObjectList(Class<T> clazz, int count) {
         List<T> collection = new ArrayList<>();
 
-        List<String> PATH_BOOK_AUTHORS = loadDataFromFile("src/resources/names.txt");
-        List<String> PATH_BOOK_TITLES = loadDataFromFile("src/resources/titles.txt");
-        List<String> PATH_CAR_MODELS = loadDataFromFile("src/resources/models.txt");
-        List<String> PATH_CAR_COLORS = loadDataFromFile("src/resources/colors.txt");
-        List<String> vegetables = loadDataFromFile("src/resources/vegetables.txt");
-
-            try{
-                if (clazz == Car.class) {
-                    String randomModel = PATH_CAR_MODELS.get(random.nextInt(PATH_CAR_MODELS.size()));
-                    Car car = new Car.Builder()
-                            .model(randomModel)
-                            .power(random.nextInt(300) + 100) // Мощность от 100 до 400
-                            .yearRelease(random.nextInt(30) + 1990) // Год от 1990 до 2020
-                            .build();
-                    collection.add(clazz.cast(car));
-                } else if (clazz == Book.class) {
-                    String randomAuthor = PATH_BOOK_AUTHORS.get(random.nextInt(PATH_BOOK_AUTHORS.size())); // Случайный автор из файла
-                    String randomTitle = PATH_BOOK_TITLES.get(random.nextInt(PATH_BOOK_TITLES.size()));
-                    Book book = new Book.Builder()
-                            .author(randomAuthor)
-                            .title(randomTitle)
-                            .numberPages(random.nextInt(500) + 100) // Количество страниц от 100 до 600
-                            .build();
-                    collection.add(clazz.cast(book));
-                } else if (clazz == Vegetable.class) {
-                    String randomColor = PATH_CAR_COLORS.get(random.nextInt(PATH_CAR_COLORS.size()));
-                    String randomVegetables = vegetables.get(random.nextInt(vegetables.size()));
-                    Vegetable vegetable = new Vegetable.Builder()
-                            .type(randomVegetables)
-                            .weight(random.nextDouble() * 10) // Вес от 0 до 10
-                            .color(randomColor)
-                            .build();
-                    collection.add(clazz.cast(vegetable));
-                } else {
-                    throw new IllegalArgumentException("Неверный тип объекта: " + clazz.getName());
-                }
-            } catch (Exception e) {
-                System.out.println("Произошла ошибка: " + e.getMessage());
+        if (clazz == Car.class) {
+            List<String> stringModelList = getRandomObjectsByCount(PATH_CAR_MODELS, count);
+            for (var i = 0; i < count; i++) {
+                Car car = new Car.Builder()
+                        .model(stringModelList.get(i))
+                        .power(random.nextInt(300) + 100)
+                        .yearRelease(random.nextInt(100) + 1886)
+                        .build();
+                collection.add(clazz.cast(car));
             }
+        } else if (clazz == Book.class) {
+            List<String> randomAuthorList = getRandomObjectsByCount(PATH_BOOK_AUTHORS, count);
+            List<String> randomTitleList = getRandomObjectsByCount(PATH_BOOK_TITLES, count);
+            for (var i = 0; i < count; i++) {
+                Book book = new Book.Builder()
+                        .author(randomAuthorList.get(i))
+                        .title(randomTitleList.get(i))
+                        .numberPages(random.nextInt(500) + 100)
+                        .build();
+                collection.add(clazz.cast(book));
+            }
+        } else if (clazz == Vegetable.class) {
+            List<String> randomNamesList = getRandomObjectsByCount(PATH_VEGETABLES_NAME, count);
+            List<String> randomColorsList = getRandomObjectsByCount(PATH_CAR_COLORS, count);
+            for (var i = 0; i < count; i++) {
+                Vegetable vegetable = new Vegetable.Builder()
+                        .type(randomNamesList.get(i))
+                        .weight(random.nextDouble() * 10)
+                        .color(randomColorsList.get(i))
+                        .build();
+                collection.add(clazz.cast(vegetable));
+            }
+        }
         return collection;
     }
 
-    private static List<String> loadDataFromFile(String filePath) {
-        List<String> data = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                data.add(line.trim());
-            }
-        } catch (IOException e) {
-            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+    private static List<String> getRandomObjectsByCount(String filePath, int count) {
+        List<String> stringList = loadDataForRandom(filePath);
+        return random.ints(0, stringList.size())
+                .limit(count)
+                .mapToObj(stringList::get)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> loadDataForRandom(String filePath) {
+        InputStream resourceAsStream = RandomObjectGenerator.class.getClassLoader().getResourceAsStream(filePath);
+
+        if (resourceAsStream == null) {
+            return Collections.emptyList();
         }
-        return data;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceAsStream))) {
+            return bufferedReader.lines()
+                    .map(String::trim)
+                    .filter(line -> !line.isEmpty())
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
     }
 }
